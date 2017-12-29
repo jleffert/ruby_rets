cda475e7615c68a9efa031c32403c62451241877
Joshua Leffert <jleffert9@gmail.com>
2017-12-28 21:58:13 -0600

    Fix redundant file loading

--- ruby_rets.rb
+++ ruby_rets.rb
@@ -1,4 +1,3 @@
-Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'lib', '*.rb')).each {|file| require "#{file}"}
 Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'lib/**', '*.rb')).each {|file| require "#{file}"}
 require 'mechanize'
 require 'pry'
