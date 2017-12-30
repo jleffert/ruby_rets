9c39bc710364f0ef80bc565194f610c5bee2754c
Joshua Leffert <jleffert9@gmail.com>
2017-12-29 22:02:58 -0600

    Refactor object parsing into one DRY method

--- lib/ruby_rets/response_parser.rb
+++ lib/ruby_rets/response_parser.rb
@@ -46,7 +46,7 @@ module RubyRETS
               if parsed_hash[current_class][child_node.name].nil?
                 parsed_hash[current_class][child_node.name] = []
               end
-              object_hash = parse_object(child_node, current_class)
+              object_hash = parse_attributes(child_node)
               parsed_hash[current_class][child_node.name] << object_hash 
             end
           end
@@ -56,26 +56,12 @@ module RubyRETS
       parsed_hash
     end
 
-    def parse_object(node, current_class, object_hash = {})
-      node.children.each do |child_node|
-        if child_node.children.length == 1 and child_node.child.text?
-           object_hash[child_node.name] = child_node.child.content
-        elsif child_node.children.any?
-          object_hash[child_node.name] = parse_nested_attribute(child_node)
-        else
-          object_hash[child_node.name] = ""
-        end
-      end
-
-      object_hash
-    end
-
-    def parse_nested_attribute(node, attribute_hash = {})
+    def parse_attributes(node, attribute_hash = {})
       node.children.each do |child_node|
         if child_node.children.length == 1 and child_node.child.text?
           attribute_hash[child_node.name] = child_node.child.content
         elsif child_node.children.any?
-          attribute_hash[child_node.name] = parse_nested_attribute(child_node)
+          attribute_hash[child_node.name] = parse_attributes(child_node, attribute_hash)
         else
           attribute_hash[child_node.name] = ""
         end
